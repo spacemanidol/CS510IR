c4f9f23aa99851e15fa99ec9c62af8f09d3c0319
Daniel Campos <spacemanidol@gmail.com>
2021-03-10 18:44:47 -0600

    Adding assignment 2 questions

--- project/scripts/get_data.sh
+++ project/scripts/get_data.sh
@@ -1,9 +1,9 @@
-wget https://msmarco.blob.core.windows.net/msmarcoranking/triples.train.small.tar.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/queries.tar.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/qrels.train.tsv
-wget https://msmarco.blob.core.windows.net/msmarcoranking/qrels.dev.tsv
-wget https://msmarco.blob.core.windows.net/msmarcoranking/orcas-doctrain-qrels.tsv.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/orcas-doctrain-queries.tsv.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/top1000.dev.tar.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/top1000.eval.tar.gz
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/triples.train.small.tar.gz
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/queries.tar.gz
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/qrels.train.tsv
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/qrels.dev.tsv
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/orcas-doctrain-qrels.tsv.gz
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/orcas-doctrain-queries.tsv.gz
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/top1000.dev.tar.gz
+wget -c https://msmarco.blob.core.windows.net/msmarcoranking/top1000.eval.tar.gz
