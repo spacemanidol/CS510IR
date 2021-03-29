63cd6fcc68b7b160caacb6fee662ae489b4c78c0
Daniel Campos <spacemanidol@gmail.com>
2021-03-29 16:32:20 -0500

    add conversion step to build index

--- competition/build_index.sh
+++ competition/build_index.sh
@@ -1,3 +1,2 @@
-python -m pyserini.index -collection JsonCollection -generator DefaultLuceneDocumentGenerator \
- -threads 1 -input integrations/resources/sample_collection_jsonl \
- -index/shared/nas/data/m1/dcampos3/clef_index -storePositions -storeDocvectors -storeRaw
+python convert_to_pyserini.py --input_dir /shared/nas/data/m1/dcampos3/clef18 --output_dir /shared/nas/data/m1/dcampos/processedclef18/
+python -m pyserini.index -collection JsonCollection -generator DefaultLuceneDocumentGenerator -threads 8 -input /shared/nas/data/m1/dcampos/processedclef18/  -index /shared/nas/data/m1/dcampos3/pyserini_indexes/clef -storePositions -storeDocvectors -storeRaw
