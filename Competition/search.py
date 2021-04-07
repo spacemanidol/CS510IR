3e93d0ec39b4b016b1f4e24391c8c4087e5d92c3
spacemanidol <dcampos3@illinois.edu>
2021-04-07 11:10:20 -0500

    updaing search

--- Competition/search.py
+++ Competition/search.py
@@ -14,7 +14,7 @@ def load_queries(filename):
         query_text = ''
         for child in query:
             if child.tag == 'id':
-                query_id = child.text
+                query_id = int(child.text[1:-1])
             if child.tag == 'en':
                 query_text = child.text
         if query_id != 0 and query_text != '':
@@ -23,39 +23,46 @@ def load_queries(filename):
 
 def load_ranker(args):
     if args.sparse and args.dense:
-        sparse_searcher = SimpleSearcher.from_prebuilt_index(args.index_path)
+        sparse_searcher = SimpleSearcher(args.index_path)
         sparse_searcher.set_bm25(args.k, args.b)
         sparse_searcher.set_rm3(args.expansion_terms, args.expansion_documents, args.original_query_weight)
         encoder = TCTColBERTQueryEncoder('castorini/tct_colbert-msmarco')
-        dense_searcher = SimpleDenseSearcher.from_prebuilt_index(args.index_path, encoder)
+        dense_searcher = SimpleDenseSearcher(args.index_path, encoder)
         hsearcher = HybridSearcher(dense_searcher, sparse_searcher)
     elif args.sparse:
-        sparse_searcher = SimpleSearcher.from_prebuilt_index(args.index_path)
-        #sparse_searcher.set_bm25(args.k, args.b)
-        #sparse_searcher.set_rm3(args.expansion_terms, args.expansion_documents, args.original_query_weight)
+        sparse_searcher = SimpleSearcher(args.index_path)
+        sparse_searcher.set_bm25(args.k, args.b)
+        sparse_searcher.set_rm3(args.expansion_terms, args.expansion_documents, args.original_query_weight)
         return sparse_searcher
     elif args.dense:
         encoder = TCTColBERTQueryEncoder('castorini/tct_colbert-msmarco')
-        dense_searcher = SimpleDenseSearcher.from_prebuilt_index(args.index_path, encoder)
+        dense_searcher = SimpleDenseSearcher(args.index_path, encoder)
         return dense_searcher
     else:
         print("Choose a valid ranking function sparse(BM25), dense(vector) or a combination of the two")
         exit(0)
 
 def main(args):
+    print("Loading Queries")
     qid2query = load_queries(args.query_file)
+    print("{} Queries loaded".format(len(qid2query)))
+    print("Loading Searcher")
     searcher = load_ranker(args)
+    print("Searcher Loaded")
+    i = 0
     with open(args.output_file, 'w') as w:
         for qid in qid2query:
             results = searcher.search(qid2query[qid])
+            i += 1
+            #print("{} quieries ranked".format(i))
             for i in range(0, args.topn):
-                w.write("{} Q0 {} {} {} {}".format(qid, results[i].docid, i, results[i].score, args.tag))
+                w.write("{} Q0 {} {} {} {}\n".format(qid, results[i].docid, i, results[i].score, args.tag))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Sech Clef Index with various values')
     parser.add_argument('--query_file', default='data/CLEF2018queries.xml')
-    parser.add_argument('--output_file', default='output')
-    parser.add_argument('--topn', default=20, type=int, help='How many documents to retrieve per query')
+    parser.add_argument('--output_file', default='candidate_run')
+    parser.add_argument('--topn', default=10, type=int, help='How many documents to retrieve per query')
     parser.add_argument('--k', default=0.9, type=float, help='K value for bm25')
     parser.add_argument('--b', default=0.4, type=float, help='B value for bm25')
     parser.add_argument('--dense', action='store_true', help='use dense ranking')
@@ -64,7 +71,6 @@ if __name__ == "__main__":
     parser.add_argument('--original_query_weight', default=1.0, type=float, help='Importance for original query')
     parser.add_argument('--sparse', action='store_true', help='use sparse ranking. Can be combined with dense')
     parser.add_argument('--tag', default="blender", type=str, help='Tag for run')  
-    parser.add_argument('--index_path', default='/shared/nas/data/m1/dcampos/pyserini_indexes/clef18/')
-    parser.add_argument('--candidate_Ranking', default='candidate.run')
+    parser.add_argument('--index_path', default='/shared/nas/data/m1/dcampos3/clef/index')
     args = parser.parse_args()
     main(args)
